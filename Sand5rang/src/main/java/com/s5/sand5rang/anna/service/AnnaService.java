ff882388bd134f4747cd552cdc96d5022d1e6edf
1azypony <105184663+1azypony@users.noreply.github.com>
2022-06-29 15:05:32 +0900

    anna rename
    rerename

--- Sand5rang/src/main/java/com/s5/sand5rang/anna/service/AnnaService.java
+++ Sand5rang/src/main/java/com/s5/sand5rang/anna/service/AnnaService.java
@@ -4,13 +4,14 @@ import org.mybatis.spring.SqlSessionTemplate;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import com.s5.sand5rang.anna.dao.AnnaDao;
 import com.s5.sand5rang.hyunsik.dao.HyunsikDao;
 
 @Service
 public class AnnaService {
 
 	@Autowired
-	private HyunsikDao hyunsikDao;
+	private AnnaDao annaDao;
 	
 	@Autowired
 	private SqlSessionTemplate sqlSession;
