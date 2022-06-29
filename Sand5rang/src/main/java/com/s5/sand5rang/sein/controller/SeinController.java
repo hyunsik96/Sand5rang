4ee02f12ff616329aec878053a069ea36ec5684b
seinni2 <104081143+seinni2@users.noreply.github.com>
2022-06-29 15:07:06 +0900

    sein_폴더 네이밍 수정
    sein_폴더 네이밍 수정

--- Sand5rang/src/main/java/com/s5/sand5rang/sein/controller/SeinController.java
+++ Sand5rang/src/main/java/com/s5/sand5rang/sein/controller/SeinController.java
@@ -3,9 +3,11 @@ package com.s5.sand5rang.sein.controller;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 
+import com.s5.sand5rang.sein.service.SeinService;
+
 @Controller
 public class SeinController {
 
 	@Autowired
-	private SeinService SeinService;
+	private SeinService seinService;
 }
