2484a302d8389bcdcf45b3193278fa33447cd2f7
hyunsik seong <hyunsig96@gmail.com>
2022-06-29 17:26:11 +0900

    프론트 폴더구조
    현식

--- Sand5rang/src/main/java/com/s5/sand5rang/hyunsik/controller/HyunsikController.java
+++ Sand5rang/src/main/java/com/s5/sand5rang/hyunsik/controller/HyunsikController.java
@@ -2,6 +2,7 @@ package com.s5.sand5rang.hyunsik.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 import com.s5.sand5rang.hyunsik.service.HyunsikService;
 
@@ -10,4 +11,9 @@ public class HyunsikController {
 
 	@Autowired
 	private HyunsikService hyunsikService;
+	
+	@RequestMapping(value="hyunsikTest.hs")
+	public String gg() {
+		return "hyunsik/focus-main/theme/index";
+	}
 }
