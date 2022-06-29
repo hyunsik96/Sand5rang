6dd943668c1124b5c3ed307bbadc3659357e516c
1azypony <105184663+1azypony@users.noreply.github.com>
2022-06-29 15:02:25 +0900

    anna rename
    anna

--- Sand5rang/src/main/java/com/s5/sand5rang/anna/dao/AnnaDao.java
+++ Sand5rang/src/main/java/com/s5/sand5rang/anna/dao/AnnaDao.java
@@ -1,9 +1,9 @@
-package com.s5.sand5rang.hyunsik.dao;
+package com.s5.sand5rang.anna.dao;
 
 import org.springframework.stereotype.Repository;
 
 @Repository
-public class HyunsikDao {
+public class AnnaDao {
 	
 
 
