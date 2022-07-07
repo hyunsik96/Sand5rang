706ae82ad6c24c78bd9417d37f52533744575856
hyunsik seong <hyunsig96@gmail.com>
2022-07-07 13:58:39 +0900

    Create INGREDIENT.sql
    현식

---
+++ group5tool/INGREDIENT.sql
@@ -0,0 +1,150 @@
+INSERT INTO INGREDIENT
+VALUES(SEQ_INGNO.NEXTVAL
+     , '파마산'
+     , 220
+     , 2
+     , 'B');
+INSERT INTO INGREDIENT
+VALUES(SEQ_INGNO.NEXTVAL
+     , '화이트'
+     , 200
+     , 2
+     , 'B');
+INSERT INTO INGREDIENT
+VALUES(SEQ_INGNO.NEXTVAL
+     , '플랫'
+     , 250
+     , 2
+     , 'B');
+     
+INSERT INTO INGREDIENT
+VALUES(SEQ_INGNO.NEXTVAL
+     , '양상추'
+     , 200
+     , 2
+     , 'V');
+INSERT INTO INGREDIENT
+VALUES(SEQ_INGNO.NEXTVAL
+     , '토마토'
+     , 100
+     , 2
+     , 'V');
+INSERT INTO INGREDIENT
+VALUES(SEQ_INGNO.NEXTVAL
+     , '오이'
+     , 30
+     , 2
+     , 'V');
+INSERT INTO INGREDIENT
+VALUES(SEQ_INGNO.NEXTVAL
+     , '양파'
+     , 50
+     , 2
+     , 'V');
+INSERT INTO INGREDIENT
+VALUES(SEQ_INGNO.NEXTVAL
+     , '피망'
+     , 20
+     , 2
+     , 'V');
+INSERT INTO INGREDIENT
+VALUES(SEQ_INGNO.NEXTVAL
+     , '할라피뇨'
+     , 70
+     , 2
+     , 'V');
+INSERT INTO INGREDIENT
+VALUES(SEQ_INGNO.NEXTVAL
+     , '아보카도'
+     , 150
+     , 2
+     , 'V');
+     
+INSERT INTO INGREDIENT
+VALUES(SEQ_INGNO.NEXTVAL
+     , '아메리칸'
+     , 100
+     , 7
+     , 'C');
+INSERT INTO INGREDIENT
+VALUES(SEQ_INGNO.NEXTVAL
+     , '모짜렐라'
+     , 100
+     , 7
+     , 'C');
+INSERT INTO INGREDIENT
+VALUES(SEQ_INGNO.NEXTVAL
+     , '슈레드'
+     , 100
+     , 7
+     , 'C');
+     
+INSERT INTO INGREDIENT
+VALUES(SEQ_INGNO.NEXTVAL
+     , '페퍼로니'
+     , 150
+     , 7
+     , 'M');
+INSERT INTO INGREDIENT
+VALUES(SEQ_INGNO.NEXTVAL
+     , '에그마요'
+     , 100
+     , 7
+     , 'M');
+INSERT INTO INGREDIENT
+VALUES(SEQ_INGNO.NEXTVAL
+     , '치킨'
+     , 170
+     , 7
+     , 'M');
+INSERT INTO INGREDIENT
+VALUES(SEQ_INGNO.NEXTVAL
+     , '쉬림프'
+     , 200
+     , 7
+     , 'M');
+INSERT INTO INGREDIENT
+VALUES(SEQ_INGNO.NEXTVAL
+     , '비프'
+     , 200
+     , 7
+     , 'M');
+INSERT INTO INGREDIENT
+VALUES(SEQ_INGNO.NEXTVAL
+     , '베이컨'
+     , 120
+     , 7
+     , 'M');
+     
+INSERT INTO INGREDIENT
+VALUES(SEQ_INGNO.NEXTVAL
+     , '랜치'
+     , 50
+     , 14
+     , 'S');
+INSERT INTO INGREDIENT
+VALUES(SEQ_INGNO.NEXTVAL
+     , '스위트칠리'
+     , 60
+     , 14
+     , 'S');
+INSERT INTO INGREDIENT
+VALUES(SEQ_INGNO.NEXTVAL
+     , '올리브오일'
+     , 70
+     , 14
+     , 'S');
+INSERT INTO INGREDIENT
+VALUES(SEQ_INGNO.NEXTVAL
+     , '머스타드'
+     , 40
+     , 14
+     , 'S');
+INSERT INTO INGREDIENT
+VALUES(SEQ_INGNO.NEXTVAL
+     , '후추'
+     , 20
+     , 14
+     , 'S');
+     
+COMMIT;
